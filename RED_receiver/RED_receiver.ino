0a9a2dbc023682442ab4e360420df273d40023e7
jineus0706 <jineus0706@naver.com>
2022-11-11 16:11:02 +0900

    0.2v

--- RED_receiver/RED_receiver.ino
+++ RED_receiver/RED_receiver.ino
@@ -1,12 +1,15 @@
 #include <SoftReset.h>
 #include <CircularQueue.h>
 #include <EEPROM.h>
+#include <avr/wdt.h>
 
 // 외부 핀 정의
 #define PIN_RELAY_1     4     // 검출 시 활성화 될 릴레이 핀
 #define PIN_RELAY_2     9     // 검출 시 활성화 될 릴레이 핀
+#define PIN_RELAY_3     14    // 릴레이 추가 A0핀
+#define PIN_RELAY_4     15    // 릴레이 추가 A1핀
 
-// 옵션
+// 부저 옵션
 #define USE_BUZZER      0     // 내부 부저(시험용) 사용 여부 설정
 #if USE_BUZZER
 #define PIN_BUZZER      10    // 검출 시 활성화 될 부저 핀. PWM 가능한 핀으로 선택할 것
@@ -22,13 +25,11 @@
 String sRecv = "";
 unsigned long tsTimeout = 0;          // 검출신호 유지시간 만료 타임스탬프
 unsigned long onTimeout = 0; 
-u16 nHoldingTime = 3000;
+u16 nHoldingTime = 3000;              // 릴레이 동작 유지시간
 u16 maxHoldingTime = 60000;
-int nHighCut = 36.111111111111111;    // 36.1111... m/s = 130 km/h, // -38.8888888..... m/s == 140 km/h; 
-// 너무 빠른 속도로 다가오는 것은 오류로 판단/제거
+int nHighCut = 36.111111111111111;    // 36.1111... m/s = 130 km/h, // -38.8888888..... m/s == 140 km/h; 너무 빠른 속도로 다가오는 것은 오류로 판단/제거
 uint8_t offDetect = true;
-int sp_alive = 0;
-int err_alive = 0;
+uint8_t rFlag = false;                // 릴레이 동작 플래그
 
 // Checksum 계산 함수
 u08 CalcCheckSum(char* str)
@@ -48,18 +49,24 @@ void setup() {
 
   pinMode(PIN_RELAY_1, OUTPUT);
   pinMode(PIN_RELAY_2, OUTPUT);
-
-  //digitalWrite(PIN_RAD, HIGH);  // switch ON uRAD
+  pinMode(PIN_RELAY_3, OUTPUT);
+  pinMode(PIN_RELAY_4, OUTPUT);
+  
   digitalWrite(PIN_RELAY_1, HIGH);
   digitalWrite(PIN_RELAY_2, HIGH);
+  digitalWrite(PIN_RELAY_3, HIGH);
+  digitalWrite(PIN_RELAY_4, HIGH);
   delay(10);
   digitalWrite(PIN_RELAY_1, LOW);
   digitalWrite(PIN_RELAY_2, LOW);
+  digitalWrite(PIN_RELAY_3, LOW);
+  digitalWrite(PIN_RELAY_4, LOW);
+
+  wdt_enable(WDTO_4S);
 }
 
 void loop() {
   char c;
-  bool error;
 
   if(Serial.available())
   {
@@ -70,56 +77,78 @@ void loop() {
         int two = sRecv.indexOf(",", first+1);
         int three = sRecv.indexOf(",", two+1);
         String command = sRecv.substring(three+1, three+2);
+        String charSpeed = sRecv.substring(first+1, two);
+        float rSpeed = charSpeed.toFloat();
+        //Serial.println(rSpeed);
 
-        if(command == "1")
-        {
-          tsTimeout = millis() + nHoldingTime;
-          if(offDetect == true)
-          {
-            onTimeout = millis() + maxHoldingTime;
-            offDetect = false;
-          }
-                   
-          digitalWrite(PIN_RELAY_1, HIGH); 
-          digitalWrite(PIN_RELAY_2, HIGH);
-          //Serial.println("Realy ON!!!!");
-          //Serial.println(tsTimeout);
+        //속도 값에 따른 릴레이 동작 조건
+        // RELAY_1 : 스피커, RELAY_2 : X(스페어), RELAY_3 : 전광판에 추돌주의 , RELAY_4 : 전광판에 안전운행
+        // 조건1: 속도 20km 이하일때, RELAY_1 : OFF / RELAY_3 : ON / RELAY_4 : OFF
+        // 조건2: 속도 21km 이상 60km이하일때, RELAY_1 : ON / RELAY_2 : OFF / RELAY_3 : ON / RELAY_4 : OFF
+        // 조건3: 속도 60km 이상 일때, RELAY_1: OFF, RELAY_2: OFF / RELAY_3 : OFF / RELAY_4 : ON
+        if(command == "1")      //레이더에서 차량 감지 했을경우에만 릴레이 동작
+        {      
+            if(rSpeed > 3 && rSpeed < 20)
+            {
+              rFlag = true;
+              digitalWrite(PIN_RELAY_1, LOW);
+              digitalWrite(PIN_RELAY_4, LOW);
+              digitalWrite(PIN_RELAY_3, HIGH);
+              tsTimeout = millis() + nHoldingTime;
+              //Serial.println("추돌주의");
+              //Serial.println(tsTimeout);
+            }
+
+            else if(rSpeed > 20 && rSpeed < 60)
+            {
+              rFlag = true;
+              digitalWrite(PIN_RELAY_4, LOW);
+              digitalWrite(PIN_RELAY_1, HIGH);
+              digitalWrite(PIN_RELAY_3, HIGH);
+              tsTimeout = millis() + nHoldingTime;
+              //Serial.println("추돌주의+사이렌");
+            }
+
+            else if(rSpeed > 60)
+            {
+              rFlag = true;
+              digitalWrite(PIN_RELAY_1,LOW);
+              digitalWrite(PIN_RELAY_3,LOW);
+              digitalWrite(PIN_RELAY_4,HIGH);
+              tsTimeout = millis() + nHoldingTime;
+              //Serial.println("안전운행");
+            }
         }
-
-        if(millis() > tsTimeout)
-        {
-          //Serial.println("Time over");
-          digitalWrite(PIN_RELAY_1, LOW); 
-          digitalWrite(PIN_RELAY_2, LOW);
-          offDetect = true;
-
-#if USE_BUZZER
-      noTone(PIN_BUZZER);
-#endif
-        }
-        
+        // 레이더 릴레이 신호가 0이면 릴레이 모두 OFF
         if(command == "0")
         {
           digitalWrite(PIN_RELAY_1, LOW); 
           digitalWrite(PIN_RELAY_2, LOW);
+          digitalWrite(PIN_RELAY_3, LOW);
+          digitalWrite(PIN_RELAY_4, LOW);
         }
-      }
+    }
       sRecv = "";
     }
     else
     {
       sRecv += c;
       //Serial.println(sRecv);
-    }
-  }
-
-  if(millis() > onTimeout && digitalRead(PIN_RELAY_1) == HIGH && digitalRead(PIN_RELAY_2 == HIGH))
+    } 
+}
+        if(millis() > tsTimeout && rFlag == true)
         {
-          Serial.println("Timeout_RESET");
-          digitalWrite(PIN_RELAY_1, LOW);
-          digitalWrite(PIN_RELAY_2, LOW);          
-          delay(3000);
-          offDetect = true;
-          soft_restart();
+          //Serial.println("Time over");
+          digitalWrite(PIN_RELAY_1, LOW); 
+          digitalWrite(PIN_RELAY_2, LOW);
+          digitalWrite(PIN_RELAY_3, LOW);
+          digitalWrite(PIN_RELAY_4, LOW);
+          rFlag = false;
+
+#if USE_BUZZER
+      noTone(PIN_BUZZER);
+#endif
         }
+        //와치독 리셋
+        wdt_reset();
 }
