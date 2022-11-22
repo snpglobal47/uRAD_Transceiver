f46d641a136d9e305416fbdfe5d0b9b611b15d24
jineus0706 <jineus0706@naver.com>
2022-11-22 10:48:35 +0900

    0.3v

--- RED_receiver/RED_receiver.ino
+++ RED_receiver/RED_receiver.ino
@@ -8,7 +8,6 @@
 #define PIN_RELAY_2     9     // 검출 시 활성화 될 릴레이 핀
 #define PIN_RELAY_3     14    // 릴레이 추가 A0핀
 #define PIN_RELAY_4     15    // 릴레이 추가 A1핀
-
 // 부저 옵션
 #define USE_BUZZER      0     // 내부 부저(시험용) 사용 여부 설정
 #if USE_BUZZER
@@ -24,12 +23,14 @@
 ///////////// 전역변수
 String sRecv = "";
 unsigned long tsTimeout = 0;          // 검출신호 유지시간 만료 타임스탬프
-unsigned long onTimeout = 0; 
-u16 nHoldingTime = 3000;              // 릴레이 동작 유지시간
+unsigned long bTimeout = 0;
+u16 nHoldingTime = 10000;              // 릴레이 동작 유지시간
+u16 bHoldingTime = 4000;              // 20km이상 60km이하 신호 유지 시간
 u16 maxHoldingTime = 60000;
 int nHighCut = 36.111111111111111;    // 36.1111... m/s = 130 km/h, // -38.8888888..... m/s == 140 km/h; 너무 빠른 속도로 다가오는 것은 오류로 판단/제거
-uint8_t offDetect = true;
 uint8_t rFlag = false;                // 릴레이 동작 플래그
+uint8_t bDetectNow = false;           // 추돌주의 플래그
+uint8_t nState = 0;
 
 // Checksum 계산 함수
 u08 CalcCheckSum(char* str)
@@ -86,37 +87,54 @@ void loop() {
         // 조건1: 속도 20km 이하일때, RELAY_1 : OFF / RELAY_3 : ON / RELAY_4 : OFF
         // 조건2: 속도 21km 이상 60km이하일때, RELAY_1 : ON / RELAY_2 : OFF / RELAY_3 : ON / RELAY_4 : OFF
         // 조건3: 속도 60km 이상 일때, RELAY_1: OFF, RELAY_2: OFF / RELAY_3 : OFF / RELAY_4 : ON
-        if(command == "1")      //레이더에서 차량 감지 했을경우에만 릴레이 동작
+        if(command == "1")      //레이더에서 차량을 감지 했을경우에만 릴레이 동작
         {      
-            if(rSpeed > 3 && rSpeed < 20)
+            if(rSpeed > 5 && rSpeed < 20)
             {
+              if(bDetectNow == false)
+              {
               rFlag = true;
+              bDetectNow = true;
               digitalWrite(PIN_RELAY_1, LOW);
               digitalWrite(PIN_RELAY_4, LOW);
               digitalWrite(PIN_RELAY_3, HIGH);
               tsTimeout = millis() + nHoldingTime;
+              bTimeout = millis() + bHoldingTime;
               //Serial.println("추돌주의");
               //Serial.println(tsTimeout);
+              }
             }
 
             else if(rSpeed > 20 && rSpeed < 60)
             {
+              if(bDetectNow == false) //&& nState != 2)
+              {
+              //nState = 2;
               rFlag = true;
+              bDetectNow = true;
               digitalWrite(PIN_RELAY_4, LOW);
               digitalWrite(PIN_RELAY_1, HIGH);
               digitalWrite(PIN_RELAY_3, HIGH);
               tsTimeout = millis() + nHoldingTime;
+              bTimeout = millis() + bHoldingTime;
               //Serial.println("추돌주의+사이렌");
+              }
             }
 
             else if(rSpeed > 60)
             {
+              if(bDetectNow == false) //&& nState != 3)
+              {
+              //nState = 3;
               rFlag = true;
+              bDetectNow = true;
               digitalWrite(PIN_RELAY_1,LOW);
               digitalWrite(PIN_RELAY_3,LOW);
               digitalWrite(PIN_RELAY_4,HIGH);
               tsTimeout = millis() + nHoldingTime;
+              bTimeout = millis() + bHoldingTime;
               //Serial.println("안전운행");
+              }
             }
         }
         // 레이더 릴레이 신호가 0이면 릴레이 모두 OFF
@@ -134,8 +152,15 @@ void loop() {
     {
       sRecv += c;
       //Serial.println(sRecv);
-    } 
+    }
 }
+
+        if(millis() > bTimeout)
+        {
+          bDetectNow = false;
+        }
+        
+        
         if(millis() > tsTimeout && rFlag == true)
         {
           //Serial.println("Time over");
@@ -144,7 +169,8 @@ void loop() {
           digitalWrite(PIN_RELAY_3, LOW);
           digitalWrite(PIN_RELAY_4, LOW);
           rFlag = false;
-
+          bDetectNow = false;
+        
 #if USE_BUZZER
       noTone(PIN_BUZZER);
 #endif
